bb10a748b9460da26b079c32737390c730a597da
Matthew Graham <mdg149@optonline.net>
2008-11-11 11:14:07 -0500

    added the obj_type.h header file to support testing includes from headers

---
+++ include/abc/obj_type.h
@@ -0,0 +1,7 @@
+#ifndef ABC_OBJ_TYPE_H
+#define ABC_OBJ_TYPE_H
+
+enum obj_type { OBJ_TYPE_1, OBJ_TYPE_2, OBJ_TYPE_3 };
+
+#endif
+
--- include/abc/object.h
+++ include/abc/object.h
@@ -1,16 +1,20 @@
 #ifndef ABC_OBJECT_H
 #define ABC_OBJECT_H
 
+#include "obj_type.h"
+
 
 class object
 {
 public:
-	object( int i );
+	object( obj_type, int i );
 
+	obj_type type() const { return m_type; }
 	int doubled() const;
 	int tripled() const;
 
 private:
+	obj_type m_type;
 	int m_value;
 };
 
