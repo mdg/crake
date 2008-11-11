e39b6e686e75a1f70ca6eaa60e3da79a6a20ca46
Matthew Graham <mdg149@optonline.net>
2008-11-11 10:22:22 -0500

    added a mock test to fake having to test something

---
+++ test/object_test.cpp
@@ -0,0 +1,19 @@
+#include "abc/object.h"
+
+
+/**
+ * This is a mock test.  Normally would use an actual testing library,
+ * but for simplicity, it's just faked here.
+ */
+void test_object()
+{
+	object o( 4 );
+
+	if ( o.doubled() != 8 ) {
+		std::cout << "object::doubled() doesn't work.\n";
+	}
+	if ( o.tripled() != 12 ) {
+		std::cout << "object::tripled() doesn't work.\n";
+	}
+}
+
