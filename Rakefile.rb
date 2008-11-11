e7a993ca6cd829325246e58775b9442fc2783ab5
Matthew Graham <mdg149@optonline.net>
2008-11-11 11:28:50 -0500

    added a test target to the Rakefile

--- Rakefile.rb
+++ Rakefile.rb
@@ -4,14 +4,20 @@ INC = FileList[ 'include/**/*.h' ]
 SRC = FileList[ 'lib/**/*.cpp' ]
 OBJ = SRC.sub( /\.cpp$/, '.o' ).sub( /^lib\//, 'obj/lib/' )
 TEST = FileList[ 'test/**/*.cpp' ]
+TEST_OBJ = TEST.sub( /\.cpp$/, '.o' ).sub( /^lib\//, 'obj/test/' )
 
 directory 'obj/lib'
+directory 'obj/test'
 
 task :default => :lib
 
-task :lib => :compile
+task :lib => :compile do
+	sh %{ar r object.a #{OBJ}}
+end
 
 task :compile => ["obj/lib"] + OBJ
+
+task :compile_test => ['obj/test'] + TEST_OBJ
 
 rule '.o' => SRC do |t|
 	sh %{g++ -c -g -Iinclude -o #{t.name} #{t.source}}
