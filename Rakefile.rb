e3782a86721ddab49db383f1bebd18a626cf0104
Matthew Graham <mdg149@optonline.net>
2008-11-11 11:45:39 -0500

    added a test target.  fixed the path where test objects are built.

--- Rakefile.rb
+++ Rakefile.rb
@@ -4,7 +4,7 @@ INC = FileList[ 'include/**/*.h' ]
 SRC = FileList[ 'lib/**/*.cpp' ]
 OBJ = SRC.sub( /\.cpp$/, '.o' ).sub( /^lib\//, 'obj/lib/' )
 TEST = FileList[ 'test/**/*.cpp' ]
-TEST_OBJ = TEST.sub( /\.cpp$/, '.o' ).sub( /^lib\//, 'obj/test/' )
+TEST_OBJ = TEST.sub( /\.cpp$/, '.o' ).sub( /^test\//, 'obj/test/' )
 
 directory 'obj/lib'
 directory 'obj/test'
@@ -13,6 +13,11 @@ task :default => :lib
 
 task :lib => :compile do
 	sh %{ar r object.a #{OBJ}}
+end
+
+task :test => [ :compile, :compile_test ] do
+	sh %{g++ -o run-test #{OBJ} #{TEST_OBJ}}
+	# sh %{g++ -o run-test #{OBJ} -Iinclude test/object_test.cpp}
 end
 
 task :compile => ["obj/lib"] + OBJ
