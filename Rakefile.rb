f13815bef032ac9ba431d77533f633dfc1fee615
Matthew Graham <mdg149@optonline.net>
2008-11-11 10:58:48 -0500

    added the Rakefile to be used for testing crake.  Currently uses standard rake tasks as crake is yet to be added.

---
+++ Rakefile.rb
@@ -0,0 +1,19 @@
+require 'rake'
+
+INC = FileList[ 'include/**/*.h' ]
+SRC = FileList[ 'lib/**/*.cpp' ]
+OBJ = SRC.sub( /\.cpp$/, '.o' ).sub( /^lib\//, 'obj/lib/' )
+TEST = FileList[ 'test/**/*.cpp' ]
+
+directory 'obj/lib'
+
+task :default => :lib
+
+task :lib => :compile
+
+task :compile => ["obj/lib"] + OBJ
+
+rule '.o' => SRC do |t|
+	sh %{g++ -c -g -Iinclude -o #{t.name} #{t.source}}
+end
+
