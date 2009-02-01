b8fc1d46ebf1583308eac2297de3e6f6dc729d99
Matthew Graham <mgraham@wgen.net>
2009-02-01 10:52:00 -0500

    new syntax experimentation in the sample rakefile

--- sample_rakefile.rb
+++ sample_rakefile.rb
@@ -1,35 +1,39 @@
 require '../crake'
 
-INC = CFileSet[ 'include/**/*.h' ]
-SRC = FileList[ 'lib/**/*.cpp' ]
-OBJ = SRC.sub( /\.cpp$/, '.o' ).sub( /^lib\//, 'obj/lib/' )
-TEST = FileList[ 'test/**/*.cpp' ]
-TEST_OBJ = TEST.sub( /\.cpp$/, '.o' ).sub( /^test\//, 'obj/test/' )
+SRC = CFileList.new()
+SRC.include( 'include' )
+SRC.compile( 'lib/**/*.cpp' )
+SRC.obj_dir = 'obj/lib'
+TEST = CFileList.new()
+TEST.include( 'include' )
+TEST.compile( 'test/**/*.cpp' )
+TEST.obj_dir = 'obj/test'
 
 
-directory 'obj/lib'
-directory 'obj/test'
+directory SRC.obj_dir
+directory TEST.obj_dir
 
 task :default => :lib
 
 task :lib => :compile do
-	sh %{ar r object.a #{OBJ}}
+	CC.archive( "object.a", SRC.objects )
 end
 
 task :test => [ :compile, :compile_test ] do
-	sh %{g++ -o run-test #{OBJ} #{TEST_OBJ}}
-	# sh %{g++ -o run-test #{OBJ} -Iinclude test/object_test.cpp}
+	CC.link( "run-test", [SRC, TEST] )
 end
 
-task :compile => ["obj/lib"] + OBJ
+task :compile => SRC.compile_tasks
 
-task :compile_test => ['obj/test'] + TEST_OBJ
+task :compile_test => TEST.compile_tasks
 
-rule '.o' => PROJECT.source do |obj|
+rule '.o' => PROJECT.dependencies( obj.name ) do |obj|
 	CC.compile( obj.name, obj.source )
 end
 
+task "program" => :compile
+
 task :clean => [] do
-	sh "rm -rf obj *.a"
+	CC.clean( PROJECT.output_files )
 end
 
