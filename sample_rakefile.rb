18f6b2f553790b6588455f4b89bbecdaea3ad2d5
Matthew Graham <mdg149@optonline.net>
2009-03-22 18:43:17 -0400

    updated the sample rakefile for more types

--- sample_rakefile.rb
+++ sample_rakefile.rb
@@ -8,6 +8,10 @@ TEST = CFileList.new()
 TEST.include( 'include' )
 TEST.compile( 'test/**/*.cpp' )
 TEST.obj_dir = 'obj/test'
+CC = CTask.new()
+CC.include( 'include' )
+CC.compile( 'lib/**/*.cpp' )
+CC.obj_dir = 'obj/lib'
 
 
 directory SRC.obj_dir
@@ -16,24 +20,31 @@ directory TEST.obj_dir
 task :default => :lib
 
 task :lib => :compile do
-	CC.archive( "object.a", SRC.objects )
+	CC.archive( "object", CC.objects )
+end
+
+task :debug do
+	CC.debug = true
 end
 
 task :test => [ :compile, :compile_test ] do
-	CC.link( "run-test", [SRC, TEST] )
+	CC.compile( 'lib/**/*.cpp' )
+	CC.obj_dir = 'obj/test'
 end
 
-task :compile => SRC.compile_tasks
+task :build do
+	CC.link( "program" )
+end
 
-task :compile_test => TEST.compile_tasks
+task :compile => CC.compile_tasks
 
-rule '.o' => PROJECT.dependencies( obj.name ) do |obj|
+rule '.o' => CC.dependencies( obj.name ) do |obj|
 	CC.compile( obj.name, obj.source )
 end
 
-task "program" => :compile
+task "program" => :build
 
 task :clean => [] do
-	CC.clean( PROJECT.output_files )
+	sh "rm -rf obj"
 end
 
