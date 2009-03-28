53ddf0fa0967175c6c83c72bd0b67c6e1b84c6a2
Matthew Graham <mdg149@optonline.net>
2009-03-28 13:13:06 -0400

    updated sample rakefile for CFileSet and CCompiler

--- sample_rakefile.rb
+++ sample_rakefile.rb
@@ -2,16 +2,14 @@ require '../crake'
 
 SRC = CFileList.new()
 SRC.include( 'include' )
-SRC.compile( 'lib/**/*.cpp' )
+SRC.compile( 'lib', 'cpp' )
 SRC.obj_dir = 'obj/lib'
 TEST = CFileList.new()
 TEST.include( 'include' )
-TEST.compile( 'test/**/*.cpp' )
+TEST.compile( 'test', 'cpp' )
 TEST.obj_dir = 'obj/test'
-CC = CTask.new()
-CC.include( 'include' )
-CC.compile( 'lib/**/*.cpp' )
-CC.obj_dir = 'obj/lib'
+
+CC = GppCompiler.new( SRC )
 
 
 directory SRC.obj_dir
@@ -19,8 +17,8 @@ directory TEST.obj_dir
 
 task :default => :lib
 
-task :lib => :compile do
-	CC.archive( "object", CC.objects )
+rule '.o' => CC.object_dependencies( obj.name ) do |obj|
+	CC.compile( obj.name )
 end
 
 task :debug do
@@ -28,23 +26,22 @@ task :debug do
 end
 
 task :test => [ :compile, :compile_test ] do
-	CC.compile( 'lib/**/*.cpp' )
-	CC.obj_dir = 'obj/test'
+	CC.files = TEST
 end
 
-task :build do
+task :compile => CC.compile_dependencies
+
+task :build => :compile do
 	CC.link( "program" )
 end
 
-task :compile => CC.compile_tasks
-
-rule '.o' => CC.dependencies( obj.name ) do |obj|
-	CC.compile( obj.name, obj.source )
+task :lib => :compile do
+	CC.archive( "object", CC.objects )
 end
 
 task "program" => :build
 
-task :clean => [] do
+task :clean do
 	sh "rm -rf obj"
 end
 
