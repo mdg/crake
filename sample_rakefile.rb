0b443c3f418892589351e4853cfd1f29ed121346
Matthew Graham <mdg149@optonline.net>
2009-03-29 19:36:44 -0400

    checkpointing the sample file
    doing more experimentation with syntax

--- sample_rakefile.rb
+++ sample_rakefile.rb
@@ -1,36 +1,59 @@
 require '../crake'
 
-SRC = CFileList.new()
-SRC.include( 'include' )
-SRC.compile( 'lib', 'cpp' )
+PROGRAM = CFileList.new()
+PROGRAM.include( 'include' )
+PROGRAM.compile( 'lib' )
 TEST = CFileList.new()
 TEST.include( 'include' )
-TEST.compile( 'test', 'cpp' )
+TEST.include( 'lib' )
+TEST.compile( 'test' )
 
 CC = GppCompiler.new()
 
 PROJECT = CProject.new()
 PROJECT.cc = CC
-PROJECT.files = SRC
-PROJECT.obj_dir = 'obj'
+# primary build
+PROJECT.compile( SRC, 'obj', false )
+PROJECT.compile( SRC ).to( 'obj' )
+# debug build
+PROJECT.compile( SRC, 'dbg', true )
+PROJECT.compile( SRC ).to( 'dbg' ).debug!
+# test build
+PROJECT.compile( SRC, 'dbg', true )
+PROJECT.compile( TEST, 'dbg', true )
+PROJECT.compile( SRC ).to( 'dbg' ).debug!
+PROJECT.compile( TEST ).to( 'dbg' ).debug!
+
+# Release and debug compilers
+RELEASE = CompileContext.new( 'obj' )
+DEBUG = CompileContext.new( 'dbg' ).debug!
+
+PROJECT.add_context( RELEASE )
+PROJECT.add_context( DEBUG )
 
 
-directory SRC.obj_dir
-directory TEST.obj_dir
+# default is to build the program
+task :default => :build
 
-task :default => :lib
+# build the program
+task "program" => :build
 
+# build & run the tests
+task :test => [ :build_test ] do
+	sh "./test_program"
+end
+
+
+
+# Rule to compile each object
 rule '.o' => PROJECT.object_dependencies( obj.name ) do |obj|
 	PROJECT.compile( obj.name )
 end
 
+# modifies any subsequent tasks to compile w/ debug options
 task :debug do
 	PROJECT.debug = true
 	PROJECT.obj_dir = 'dbg'
-end
-
-task :test => [ :compile, :compile_test ] do
-	PROJECT.files = TEST
 end
 
 task :compile => PROJECT.compile_dependencies
@@ -39,13 +62,16 @@ task :build => :compile do
 	PROJECT.link( "program" )
 end
 
+task :build_test => [ :debug, :compile_test ] do
+	PROJECT.link( "test_program", 'testpp' )
+end
+
 task :lib => :compile do
 	PROJECT.archive( "object", CC.objects )
 end
 
-task "program" => :build
-
 task :clean do
 	sh "rm -rf obj"
+	sh "rm -rf dbg"
 end
 
