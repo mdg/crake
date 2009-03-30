4838917256e4dfc0505df22271cd5a0c9c17958f
Matthew Graham <mdg149@optonline.net>
2009-03-30 00:27:32 -0400

    removed old-style code
    simplifying things into CTargets.  removing old CFileLists and
    CompileContext objects that were causing complexity.
    removed context modifiers.  context will be stated explicitly or matched
    to one of the CTargets based on the object path

--- sample_rakefile.rb
+++ sample_rakefile.rb
@@ -1,27 +1,5 @@
 require '../crake'
 
-# application source
-APP_SRC = CFileList.new()
-APP_SRC.include( 'include' )
-APP_SRC.compile( 'lib' ).exclude!( 'main.cpp' )
-# main is separate for not including in the test app
-APP_MAIN = CFileList.new()
-APP_MAIN.include( 'include' )
-APP_MAIN.compile( 'lib' ).filter!( 'main.cpp' )
-# test source
-TEST_SRC = CFileList.new()
-TEST_SRC.include( 'include' )
-TEST_SRC.include( 'lib' )
-TEST_SRC.compile( 'test' )
-
-# Group files into targets
-APP_FILES = [ APP_SRC, APP_MAIN ]
-TEST_FILES = [ APP_SRC, TEST_SRC ]
-
-# Release and debug compilers
-RELEASE = CompileContext.new( 'obj' )
-DEBUG = CompileContext.new( 'dbg' ).debug!
-
 # The compiler class, RELEASE is the default context
 CC = GppCompiler.new()
 CC.context = RELEASE
@@ -59,19 +37,6 @@ task :build_test => "test_program"
 task :test => [ :test_context, :build ] do
 	sh "./test_program"
 end
-
-
-# Context modifiers
-# modifies any subsequent tasks to compile w/ debug options
-task :debug do
-	CC.context = DEBUG
-end
-
-# modifies the file to build
-task :test_files do
-	CC.files = TEST_FILES
-end
-
 
 
 # Rule to compile each object
